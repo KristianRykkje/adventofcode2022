3b7fac002b6e5bbf484ddbbcb13f0fba28df6f36
KristianRykkje <kriryk@gmail.com>
2022-12-16 14:53:16 +0100

    cant be bothered

--- day11/index.js
+++ day11/index.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
 const data = fs.readFileSync("data.txt", "utf8");
 
-const commandsAndOutput = data.split("\n");
+const monkies = data.split("\n\n");
+
+class Monkey {
+  constructor(name, items, operation, test) {
+    this.name = name;
+    this.items = items;
+    this.operation = operation;
+    this.test = test;
+  }
+}
+
+monkies.forEach((monkey) => {
+  const [name, items, operation, test] = monkey.split("\n");
+  const monkeyObj = new Monkey(name, items, operation, test);
+  console.log("🚀 ~ file: index.js:44 ~ monkeyObj", monkeyObj);
+}
