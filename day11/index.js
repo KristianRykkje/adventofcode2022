c645f3d72240008dd10935d54fb8db0810c9cdc7
KristianRykkje <kriryk@gmail.com>
2022-12-18 08:51:48 +0100

    ......

--- day11/index.js
+++ day11/index.js
@@ -1,21 +1,39 @@
 const fs = require("fs");
 const data = fs.readFileSync("data.txt", "utf8");
 
-const monkies = data.split("\n\n");
+const monkiesRaw = data.split("\n\n");
 
 class Monkey {
-  constructor(name, items, operation, test) {
+  constructor(name, items, operation, test, number, divisibleBy) {
     this.name = name;
     this.items = items;
     this.operation = operation;
     this.test = test;
+    this.inspected = 0;
+    this.number = number;
+    this.divisibleBy = divisibleBy;
+  }
+
+  inspect() {
+    this.inspected++;
+  }
+
+  addItems(items) {
+    this.items.push(...items);
+  }
+
+  removeItems(items) {
+    this.items = this.items.filter((item) => !items.includes(item));
   }
 }
 
-monkies.forEach((monkey) => {
+const monkies = [];
+
+monkiesRaw.forEach((monkey) => {
   const [nameRaw, itemsRaw, operationRaw, ...testRaw] = monkey.split("\n");
 
   const name = nameRaw.split(":")[0];
+  const number = +name.split(" ")[1];
 
   const items = itemsRaw
     .split(":")[1]
@@ -47,10 +65,36 @@ monkies.forEach((monkey) => {
       "}\n" +
       "}"
   );
+  const divisibleBy = +divText.split(" ").pop();
 
-  const monkeyObj = new Monkey(name, items, operation, test);
-  console.log(
-    "🚀 ~ file: index.js:54 ~ monkies.forEach ~ monkeyObj",
-    monkeyObj
-  );
+  monkies.push(new Monkey(name, items, operation, test, number, divisibleBy));
 });
+
+const base = monkies.reduce((prev, monkey) => {
+  return prev * monkey.divisibleBy;
+}, 1);
+
+for (let i = 0; i < 20; i++) {
+  for (let j = 0; j < monkies.length; j++) {
+    const monkey = monkies[j];
+    const itemsToRemove = [];
+    for (let k = 0; k < monkey.items.length; k++) {
+      const itemValue = monkey.items[k];
+      itemsToRemove.push(itemValue);
+      const worryLevel = itemValue % base;
+      // const worryLevelB = monkey.operation(itemValue);
+      // const worryLevel = (worryLevelB - (worryLevelB % 3)) / 3;
+      monkey.inspect();
+      const monkeyToPassTo = monkey.test(worryLevel);
+      const monkeyToPassToObj = monkies.find(
+        (monkey) => monkey.number === monkeyToPassTo
+      );
+      monkeyToPassToObj.addItems([worryLevel]);
+    }
+    monkey.removeItems(itemsToRemove);
+  }
+}
+
+const monkiesSorted = monkies.sort((a, b) => b.inspected - a.inspected);
+// console.log(monkiesSorted[0].inspected, monkiesSorted[1].inspected);
+console.log(monkiesSorted[0].inspected * monkiesSorted[1].inspected);
